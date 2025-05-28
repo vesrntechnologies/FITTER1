e2dbfc4e27cec59f6bfb192b8149e8307ac6c29a
vesrntechnologies <vennjoy@gmail.com>
2025-05-28 13:36:37 +0530

    Add files via upload

---
+++ js/questions.js
@@ -0,0 +1,635 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "What is the name of the safety sign as shown in the figure?",
+    immg:"img1",
+    answer: "Information Sign",
+    options: [
+      "Warn Sign",
+      "Mandatory Sign",
+      "Information Sign",
+      "Danger Sign"
+    ]
+  },
+    {
+    numb: 2,
+    question: "What is smothering in extinguishing of fire?",
+    answer: "Isolating the fire from the supply of oxygen",
+    options: [
+      "Adding the fuel element to the fire",
+      "Removing the fuel element from the fire",
+      "Using of water to lower the temperature", 
+      "Isolating the fire from the supply of oxygen"
+    ]
+  },
+    {
+    numb: 3,
+    question: "Which step of the 5s-concept refers Standardization?",
+    
+    answer: "Step-4",
+    options: [
+      "Step-1",
+      "Step-2",
+      "Step-3",
+      "Step-4"
+    ]
+  },
+    {
+    numb: 4,
+    question: "Which pliers is used for making the wire hooks and loops?",
+    answer: "Round nose pliers",
+    options: [
+      "Flat nose pliers",
+      "Long nose pliers",
+      "Round nose pliers",
+      "Diagonal cutting pliers"
+    ]
+  },
+    {
+    numb: 5,
+    question: "What is the use of the pincer?",
+    answer: "Extracting the pin nails from the wood",
+    options: [
+      "Twisting the flexible wires",
+      "Cutting larger diameter of wires",
+      "Extracting the pin nails from the wood",
+      "Holding small objects, where finger cannot reach"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Which plier is used for making wire hooks and loops?",
+    answer: "Yellow",
+    options: [
+      "Blue",
+      "White",
+      "Yellow",
+      "Green"
+    ]
+  },
+  {
+    numb: 7,
+    question: "What is the use of pincer? ",
+    answer: "Incineration",
+    options: [
+      "Landfill",
+      "Composting",
+      "Incineration",
+      "Water compaction"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Which disposal method of waste save lot of energy?",
+   
+    answer: "Recycling",
+    options: [
+      "Land fill",
+      "Recycling",
+      "Incineration",
+      "Composting"
+    ]
+  },
+      {
+    numb: 9,
+    question: "What is the name of the tool as shown in the figure?",
+    
+    answer: "Diagonal cutting pliers",
+    immg:"img9",
+    options: [
+      "Nose pliers",
+      "Crimping tool",
+      "Combination plier",
+      "Diagonal cutting pliers"
+    ]
+  },
+    {
+    numb: 10,
+    question: "What is the name of the file as shown in the figure?",
+    immg:"img10",
+    answer: "Rasp cut file",
+    options: [
+      "Curved cut file",
+      "Single cut file ",
+      "Double cut file",
+      "Rasp cut file"
+    ]
+  },
+     {
+    numb: 11,
+    question: "What is the name of the drill bit as shown in the figure?",
+    immg:"img11",
+    answer: "Taper shank bit",
+    options: [
+      "Flat drill bit",
+      "Taper shank bit",
+      "countersink bit",
+      "straight shank bit"
+    ]
+  },
+     {
+    numb: 12,
+    question: "Which file is used for sharpening the blunt teeth of a tenon saw?",
+    
+    answer: "Triangular file",
+    options: [
+      "square file",
+      "round file",
+      "Triangular file",
+      "half round file"
+    ]
+  },
+ {
+    numb: 13,
+    question: "What is the name of the part marked ‘X’ as shown in the figure?",
+    immg:"img13",
+    answer: "Heel",
+    options: [
+      "Edge",
+      "Tang",
+      "Heel",
+      "point"
+    ]
+  },
+{
+    numb: 14,
+    question: "What is the name of the hammer as shown in the figure?",
+    
+    answer: "claw hammer",
+     immg:"img14",
+    options: [
+      "claw hammer",
+      "Ball pein hammer",
+      "Cross pein hammer",
+      "Straight pein hammer"
+    ]
+  },
+    {
+    numb: 15,
+    question: "What is the name of the tool as shown in the figure?",
+     immg:"img15",
+    answer: "Ball pein hammer",
+    options: [
+      "Claw hammer",
+      "Tack hammer",
+      "cross pein hammer",
+     "Ball pein hammer"
+    ]
+  },
+    {
+    numb: 16,
+    question: "Which is the physical hazard?",
+    
+    answer: "Vibration",
+    options: [
+      "smoking",
+      "Vibration",
+      "corrosive",
+      "bacteria"
+    ]
+  },
+    {
+    numb: 17,
+    question: "Which type of fire extinguisher is used for fire on electrical equipment?",
+    
+    answer: "Halon type",
+    options: [
+      "Halon type",
+      "Foam type",
+      "Gas cartridge water type",
+      "Stored pressure water type"
+    ]
+  },
+     {
+    numb: 18,
+    question: "What is the name of the tool as shown in the figure?",
+      immg:"img18",
+    answer:"pincer",
+    options: [
+      "wire stripper",
+      "slide cutter",
+      "pincer",
+      "gimlet"
+    ]
+  },
+       {
+    numb: 19,
+    question: "Which is the correct sequence of operation to be performed while using the fire extinguisher?",
+    
+    answer:"Pull, Aim, Squeeze, Sweep",
+    options: [
+      "Pull, Aim, Squeeze, Sweep",
+      "Pull, Aim, Sweep, Squeeze",
+      "Push, Arrange, Squeeze, Sweep",
+      "Push, Arrange, Sweep, Sequence"
+    ]
+  },
+        {
+    numb: 20,
+    question: "Which Personal Protective Equipment (PPE) is used for the protection from fumes?",
+   
+    answer:"Respirator",
+    options: [
+      "Apron",
+      "Goggles",
+      "Ear muff",
+      "Respirator"
+    ]
+  },  
+    
+             {
+    numb: 21,
+    question: "What is starving in extinguishing of fire?",
+    
+    answer:"Removing fuel element from the fire",
+    options: [
+      "Adding fue to the fire",
+      "Using water to cool the fire ",
+      "Removing fuel element from the fire",
+      "Preventing oxygen supply to the fire"
+    ]
+  },
+                 
+        {
+    numb: 22,
+    question: "Which artificial respiration method is to be performed to the victim with injuries on the chest and belly?",
+    
+    answer:"Mouth to mouth method",
+    options: [
+      "Schafers method",
+      "Mouth to mouth method",
+      "Holger Nelson back pressure method",
+      "Nelson arm lift back pressure method"
+    ]
+  },  
+     {
+    numb: 23,
+    question: "Which type of occupational health hazard is cause for infection due to bacteria?",
+    
+    answer:"Biological hazard",
+    options: [
+      "Electrical Hazard",
+      "Physiological hazard",
+      "Biological hazard",
+      "Pschological hazard"
+    ]
+  }, 
+     {
+    numb: 24,
+    question: "Which is the golden hour for the victim injured on head with risk of dying?",
+    
+    answer: "First 30 minutes",
+    options: [
+      "First 15 minutes",
+      "First 30 minutes",
+      "First 45 minutes",
+     "First 60 minutes"
+    ]
+  },
+  {
+    numb: 25,
+    question: "Which condition of the victim is referred as COMA stage?",
+    
+    answer: "Lie totally senseless and do not respond to calls",
+    options: [
+      "Un consscious but can respond to calls",
+      "Consicous but cannot resopnd to call",
+      "Breathing and respond to call",
+     "Lie totally senseless and do not respond to calls"
+    ]
+  },        
+               {
+    numb: 26,
+    question: "Which criteria must be satisfied for the quality of PPE s?",
+    
+    answer:"It should with stand the hazards",
+    options: [
+      "It should with stand the hazards",
+      "It cannot be used for long period",
+      "made by non standard material",
+      "low cost"
+    ]
+  }, 
+     {
+    numb: 27,
+    question: "What is the possible range to measure the size of the wire in a Standard Wire Gauge(SWG)?",
+    
+    answer: "0-36",
+    options: [
+      "0-44",
+      "0-42",
+      "0-38",
+     "0-36"
+    ]
+  },
+          {
+    numb: 28,
+    question: "What is the name of the wire joint as shown in the figure?",
+    immg:"img28",
+    answer: "Knotted tap joint",
+    options: [
+      "Aerial tap joint",
+      "Knotted tap joint",
+      "Duplex cross tap joint",
+     "Double cross tap joint"
+    ]
+  },
+
+      {
+    numb: 29,
+    question: "What is the full form of XLPE Cable?",
+    
+    answer: "Cross Linked Poly Ethylene",
+    options: [
+      "Cross Line Poly Ethylene",
+      "X ess Line Phase Earthing",
+      "Cross Linked Poly Ethylene",
+     "Excess Length Paper and Ebonite"
+    ]
+  },
+      {
+    numb: 30,
+    question: "How many electrons are there in the valence shell of a copper atom?",
+    
+    answer: "1",
+    options: [
+      "1",
+      "2",
+      "8",
+     "18"
+    ]
+  },
+      {
+    numb: 31,
+    question: "Which effect of the electric current takes place in the neon lamp?",
+    
+    answer: "Gas ionization effect",
+    options: [
+      "Heating effect",
+      "Magnetic effect",
+      "Chemical effect",
+     "Gas ionization effect"
+    ]
+  },
+      {
+    numb: 32,
+    question: "What is the unit of insulation resistance?",
+    
+    answer: "Mega ohm",
+    options: [
+      "Ohm",
+      "Kilo ohm",
+      "Milli Ohm",
+     "Mega ohm"
+    ]
+  },
+     {
+    numb: 33,
+    question: "Which type of soldering flux is used for soldering aluminium conductors?",
+    
+    answer: "Kynal flux",
+    options: [
+      "Tallow",
+      "Kynal flux",
+      "Zinc Chloride",
+     "Sal ammonia"
+    ]
+  },
+      {
+    numb: 34,
+    question: "What is the name of the part marked X in UG cables as shown in the figure?",
+    
+    answer: "Lead sheath",
+    immg:"img34",
+    options: [
+      "Serving",
+      "Bedding",
+      "Armouring",
+     "Lead sheath"
+    ]
+  },
+     {
+    numb: 35,
+    question: "What is the name of the joint as shown in the figure?",
+    
+    answer: "Married joint",
+    immg:"img35",
+    options: [
+      "Married joint",
+      "Scarfed Joint",
+      "Western Union Joint",
+     "Britannia Sraight Joint"
+    ]
+  },
+     {
+    numb: 36,
+    question: "What is the use of Britannia T joint?",
+    
+    answer: "Tapping the service connection from overhead lines",
+    
+    options: [
+      "Extending the length of the lines",
+      "Inside and outside wiring installation",
+      "Mechanical stress not required on conductor",
+     "Tapping the service connection from overhead lines"
+    ]
+  },
+     {
+    numb: 37,
+    question: "What is the name of the part marked X of the belted U.G cable as shown in the figure?",
+    
+    answer: "Lead sheath",
+    immg:"img37",
+    options: [
+      "Jute filling",
+      "Armouring",
+      "Lead sheath",
+     "Paper insulation"
+    ]
+  },
+     {
+    numb: 38,
+    question: "What is the purpose of the bedding insulation in the U.G. cable?",
+    
+    answer: "Protect the metallic sheath against corrosion",
+ 
+    options: [
+      "Protect the cable fromn mechanical injury",
+      "Protect the cable from moisture and gases",
+      "Protect armouring from atomspheric condition",
+     "Protect the metallic sheath against corrosion"
+    ]
+  },
+      {
+    numb: 39,
+    question: "What are the two classifications of system of units?",
+    
+    answer: "Fundamental and derived",
+ 
+    options: [
+      "British and Metric",
+      "Gravitational and non gravitational",
+      "Fundamental and derived",
+     "Metric and International"
+    ]
+  },
+      {
+    numb: 40,
+    question: "What are fundamental units?",
+    
+    answer: "Length, Mass, Time",
+ 
+    options: [
+      "Length, Mass, Volume",
+      "Length, Mass, Time",
+      "Length, Mass, Area",
+     "Length, Pressure, Volume"
+    ]
+  },
+       {
+    numb: 41,
+    question: "What denotes letter ' in MKS system?",
+    
+    answer: "Meter",
+ 
+    options: [
+      "Mile",
+      "Milli Meter",
+      "Meter",
+     "Micron"
+    ]
+  },
+
+       {
+    numb: 42,
+    question: "How many millimetres are there in 1 inch?",
+    
+    answer: "25.4mm",
+ 
+    options: [
+      "2.54mm",
+      "25.4mm",
+      "24.5mm",
+     "2.45mm"
+    ]
+  },
+      {
+    numb: 43,
+    question: "Convert decimal 0.000659 to fraction?",
+    
+    answer: "659/1000000",
+ 
+    options: [
+      "659/10000",
+      "659/100000",
+      "659/1000000",
+     "659/1000"
+    ]
+  },
+    {
+    numb: 44,
+    question: "What is the square root of 529?",
+    
+    answer: "23",
+ 
+    options: [
+      "13",
+      "23",
+      "33",
+     "43"
+    ]
+  },
+     {
+    numb: 45,
+    question: "Identify the conventional symbol of material?",
+    immg:"img45",
+    answer: "Water",
+ 
+    options: [
+      "Water",
+      "Concrete",
+      "Asbestos",
+     "Glass"
+    ]
+  },
+     {
+    numb: 46,
+    question: "Identify the conventional symbol of material?",
+    immg:"img46",
+    answer: "Wood",
+ 
+    options: [
+      "Lead",
+      "Glass",
+      "Wood",
+     "Paper"
+    ]
+  },
+     {
+    numb: 47,
+    question: "Identify the name of instrument?",
+    immg:"img47",
+    answer: "Protractor",
+ 
+    options: [
+      "Set Square",
+      "Protractor",
+      "French curve",
+     "T Square"
+    ]
+  },
+      {
+    numb: 48,
+    question: "Identify the name of set square?",
+    immg:"img48",
+    answer: "45o set square",
+ 
+    options: [
+      "15o Set Square",
+      "60o set square",
+      "30o set square",
+     "45o set square"
+    ]
+  },
+      {
+    numb: 49,
+    question: "Compass used to draw for___.",
+    
+    answer: "circle",
+ 
+    options: [
+      "rectangle",
+      "square",
+      "circle",
+     "triangle"
+    ]
+  },
+      {
+    numb: 50,
+    question: "a+b = 15, a-b=10 Then 4ab=? ",
+    
+    answer: "125",
+ 
+    options: [
+      "125",
+      "100",
+      "120",
+     "135"
+    ]
+  },
+  // you can uncomment the below codes and make duplicate as more as you want to add question
+  // but remember you need to give the numb value serialize like 1,2,3,5,6,7,8,9.....
+
+  //   {
+  //   numb: 6,
+  //   question: "Your Question is Here",
+  //   answer: "Correct answer of the question is here",
+  //   options: [
+  //     "Option 1",
+  //     "option 2",
+  //     "option 3",
+  //     "option 4"
+  //   ]
+  // },
+];
